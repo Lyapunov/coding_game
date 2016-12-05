42616aa037e89fe40967de2a510063e4002c6fed
Lyapunov <vaskorichard@gmail.com>
2016-12-05 14:18:36 +0100

    An improved version.

--- mini_perl_12.pl
+++ mini_perl_12.pl
@@ -1,2 +1,2 @@
 #!/usr/bin/perl
-<>;<>;while(<>){$i=$r=0;$z[$i++]+=abs,$r+=abs,$k=($k,abs)[$k<abs]for/\S+/g;$s=$s>$r?$s:$r}print+(sort{$b<=>$a}@z)[0]," $s $k"
+<>;<>;while(<>){$i=$r=0;$z[$i++]+=$_,$r+=$_,$k=($k,$_)[$k<$_]for/\d+/g;$s=$r if$r>$s}print+(sort{$b<=>$a}@z)[0]," $s $k"
