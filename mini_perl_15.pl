84a5a38d39788c71ce0c810003df8d9ba40f8248
Lyapunov <vaskorichard@gmail.com>
2016-12-12 02:55:37 +0100

    Improving.

--- mini_perl_15.pl
+++ mini_perl_15.pl
@@ -1,3 +1,3 @@
 #!/usr/bin/perl
-@z=reverse@x=sort(@y=<>=~/\d/g);print"@x"eq"@y"||"@z"eq"@y"?false:true
+@z=reverse@x=sort@y=<>=~/\d/g;print"@x"eq"@y"||"@z"eq"@y"?false:true
 #23456789 123456789 123456789 123456789 123456789 123456789 123456789 123456789
